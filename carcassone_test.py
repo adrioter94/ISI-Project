8cb16c6db6a761019f8cf5b440b7b7a0628e2dd1
arotena <albertosss27@gmail.com>
2016-12-29 20:03:39 +0100

    test para un array de 18 piezas

--- carcassone_test.py
+++ carcassone_test.py
@@ -1,4 +1,5 @@
 from Fichas import Fichas
+from Fichas import ArrayFichas
 import unittest
 class CarcassoneTest(unittest.TestCase):
 	def test_first_ficha(self):
@@ -21,7 +22,99 @@ TTT
 		self.assertEqual(expected,Fichas('T','T','T','T'
 ,'T','T','T','P','P','P','P','P','P').imprimir())
 
-	
+	def test_create_array_of_18(self):
+		expected ="""TTT
+TIT
+TIT
+TIT
+TTT
+TTT
+TTT
+TTT
+TTT
+PPP
+TTT
+TTT
+TTT
+TTT
+PPP
+TTT
+TTT
+TTT
+TTT
+PPP
+TTT
+TTT
+TTT
+TTT
+PPP
+PPP
+TTT
+TTT
+TTT
+PPP
+PPP
+TTT
+TTT
+TTT
+PPP
+PPP
+TTT
+TTT
+TTT
+PPP
+TTT
+PPT
+PPT
+PPT
+PPP
+TTT
+PPT
+PPT
+PPT
+PPP
+TTT
+PPT
+PPT
+PPT
+PPP
+TTT
+PPT
+PPT
+PPT
+PPP
+TTT
+PPT
+PPT
+PPT
+PPP
+TTT
+TPP
+TPP
+TPP
+PPP
+TTT
+TPP
+TPP
+TPP
+PPP
+PPP
+TPT
+TPT
+TPT
+PPP
+PPP
+TPT
+TPT
+TPT
+PPP
+PPP
+TPT
+TPT
+TPT
+PPP
+"""
+		self.assertEqual(expected,ArrayFichas().imprimir())
 
 if __name__ == '__main__':
 	unittest.main()
