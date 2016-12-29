10f35c8f260b0ed19dcaeef94736bf47c83669f9
arotena <albertosss27@gmail.com>
2016-12-29 19:41:28 +0100

    test para varias fichas

--- carcassone_test.py
+++ carcassone_test.py
@@ -7,7 +7,19 @@ PPP
 PPP
 PPP
 TTT"""
-		self.assertEqual(expected,Fichas().imprimir())
+		self.assertEqual(expected,Fichas('P','T','T','T'
+,'T','T','T','P','P','P','P','P','P').imprimir())
+
+def test_second_ficha(self):
+		expected="""TTT
+PTP
+PTP
+PTP
+TTT"""
+		self.assertEqual(expected,Fichas('T','T','T','T'
+,'T','T','T','P','P','P','P','P','P').imprimir())
+
+	
 
 if __name__ == '__main__':
 	unittest.main()
