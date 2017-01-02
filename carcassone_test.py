5244ab8eab08d9190ff71498ffe49d8635528e75
arotena <albertosss27@gmail.com>
2017-01-02 13:57:21 +0100

    añadido el test para la matriz del tablero

--- carcassone_test.py
+++ carcassone_test.py
@@ -120,6 +120,18 @@ PPP
 		self.assertEqual(72,len(Tablero().tablero))
 	def test_tablero_columnas(self):
 		self.assertEqual(72,len(Tablero().tablero[0]))
+	def test_tablero_matriz(self):
+		i = 0
+		z = 0
+		expected = ""
+		while z < 72:
+			while i < 72:
+				expected += 'X'
+				i = i + 1
+			i = 0
+			expected += '\n'
+			z = z +1
+		self.assertEqual(expected,Tablero().imprimir_tablero())
 
 if __name__ == '__main__':
 	unittest.main()
