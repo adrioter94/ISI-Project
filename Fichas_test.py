ae7ff40b985eeb35c299976779a9a2691e2f49d4
scantero <sm.cantero@gmail.com>
2017-01-17 17:50:08 +0100

    test_ficha_6

--- Fichas_test.py
+++ Fichas_test.py
@@ -52,6 +52,15 @@ PCP
 """
 		self.assertEqual(expected, Fichas('I','P','P','P','P','C','P','P','P','P','P','P','P').imprimir())
 
+	def test_ficha_6(self):
+		expected="""AAA
+PPP
+PPC
+PPP
+PCP
+"""
+		self.assertEqual(expected, Fichas('P','A','A','A','P','C','P','P','C','P','P','P','P').imprimir())
+
 # 	def test_create_array_of_23(self):
 # 		expected ="""AAA
 # AAA
