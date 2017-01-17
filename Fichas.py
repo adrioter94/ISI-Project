42e7420e4d7cea426649a2db9059c3390cdd73cd
scantero <sm.cantero@gmail.com>
2017-01-17 17:57:25 +0100

    Ficha correspondiente test_ficha_8

--- Fichas.py
+++ Fichas.py
@@ -61,6 +61,8 @@ class ArrayFichas:
 			self.fichas.append(Fichas('P','A','A','A','P','C','P','P','C','P','P','P','P'))
 		for i in range(3):
 			self.fichas.append(Fichas('P','A','A','A','P','C','P','P','P','P','P','C','P'))
+		for i in range(3):
+			self.fichas.append(Fichas('X','A','A','A','P','C','P','P','C','P','P','C','P'))
 
 	def imprimir(self):
 		resultado = ''
