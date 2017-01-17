c5a9e42ff6a535560ba2d3d9370f7fa1abb4a615
scantero <sm.cantero@gmail.com>
2017-01-17 17:37:10 +0100

    test_ficha_3

--- Fichas_test.py
+++ Fichas_test.py
@@ -24,125 +24,136 @@ AAA
 		self.assertEqual(expected,Fichas('A','A','A','A'
 ,'A','A','A','P','P','P','P','P','P').imprimir())
 
-	def test_create_array_of_23(self):
-		expected ="""AAA
+
+	def test_ficha_3(self):
+		expected="""AAA
 AAA
 AAA
 AAA
-AAA
-AAA
-AAA
-AAA
-AAA
-PPP
-AAA
-AAA
-AAA
-AAA
-PPP
-AAA
-AAA
-AAA
-AAA
-PPP
-AAA
-AAA
-AAA
-AAA
-PPP
-PPP
-AAA
-AAA
-AAA
-PPP
-PPP
-AAA
-AAA
-AAA
-PPP
-PPP
-AAA
-AAA
-AAA
-PPP
-AAA
-PPA
-PPA
-PPA
-PPP
-AAA
-PPA
-PPA
-PPA
-PPP
-AAA
-PPA
-PPA
-PPA
-PPP
-AAA
-PPA
-PPA
-PPA
-PPP
-AAA
-PPA
-PPA
-PPA
-PPP
-AAA
-APP
-APP
-APP
-PPP
-AAA
-APP
-APP
-APP
-PPP
-PPP
-APA
-APA
-APA
-PPP
-PPP
-APA
-APA
-APA
-PPP
-PPP
-APA
-APA
-APA
-PPP
-AAA
-PPP
-PPP
-PPP
-PPP
-AAA
-PPP
-PPP
-PPP
-PPP
-AAA
-PPP
-PPP
-PPP
-PPP
-AAA
-PPP
-PPP
-PPP
-PPP
-AAA
-PPP
-PPP
-PPP
-PPP
+PCP
 """
-		
-		self.assertEqual(expected,ArrayFichas().imprimir())
+		self.assertEqual(expected, Fichas('A','A','A','A','P','C','P','A','A','A','A','A','A').imprimir())
+
+# 	def test_create_array_of_23(self):
+# 		expected ="""AAA
+# AAA
+# AAA
+# AAA
+# AAA
+# AAA
+# AAA
+# AAA
+# AAA
+# PPP
+# AAA
+# AAA
+# AAA
+# AAA
+# PPP
+# AAA
+# AAA
+# AAA
+# AAA
+# PPP
+# AAA
+# AAA
+# AAA
+# AAA
+# PPP
+# PPP
+# AAA
+# AAA
+# AAA
+# PPP
+# PPP
+# AAA
+# AAA
+# AAA
+# PPP
+# PPP
+# AAA
+# AAA
+# AAA
+# PPP
+# AAA
+# PPA
+# PPA
+# PPA
+# PPP
+# AAA
+# PPA
+# PPA
+# PPA
+# PPP
+# AAA
+# PPA
+# PPA
+# PPA
+# PPP
+# AAA
+# PPA
+# PPA
+# PPA
+# PPP
+# AAA
+# PPA
+# PPA
+# PPA
+# PPP
+# AAA
+# APP
+# APP
+# APP
+# PPP
+# AAA
+# APP
+# APP
+# APP
+# PPP
+# PPP
+# APA
+# APA
+# APA
+# PPP
+# PPP
+# APA
+# APA
+# APA
+# PPP
+# PPP
+# APA
+# APA
+# APA
+# PPP
+# AAA
+# PPP
+# PPP
+# PPP
+# PPP
+# AAA
+# PPP
+# PPP
+# PPP
+# PPP
+# AAA
+# PPP
+# PPP
+# PPP
+# PPP
+# AAA
+# PPP
+# PPP
+# PPP
+# PPP
+# AAA
+# PPP
+# PPP
+# PPP
+# PPP
+# """
+#
+# 		self.assertEqual(expected,ArrayFichas().imprimir())
+
 
 	def test_girar_ficha(self):
 		f=Fichas('A','A','A','A','P','P','P','A','A','A','A','A','A')
