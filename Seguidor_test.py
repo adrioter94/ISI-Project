e6f3abcf5af8892f03afca2d9082d0f06b023516
Diego Payo Martinez <dpayomartinez@gmail.com>
2017-01-18 09:45:11 +0100

    Añado test para un metodo que me devuelva todos los posibles seguidores (colores) que hay en el juego max.5

--- Seguidor_test.py
+++ Seguidor_test.py
@@ -9,5 +9,20 @@ class Seguidor_test(unittest.TestCase):
         self.assertEqual(expected,Seguidor().dame_seguidor("rojo"));
 
 
+    def test_dame_seguidor_verde (self):
+           expected="S verde\n"
+           self.assertEqual(expected,Seguidor().dame_seguidor("verde"));
+
+
+   # Test color que no existe en los seguidores
+    def test_seguidor_color_no (self):
+        expected="color no existe"
+        self.assertEqual(expected,Seguidor().dame_seguidor("rosa"));
+
+    # Test que muestra los colores de los seguidores posibles    
+    def test_dame_todos(self):
+       expected="S rojo\nS verde\nS azul\nS amarillo\nS negro\n";
+
+
 if __name__ == '__main__':
     unittest.main()
