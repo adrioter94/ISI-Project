089a928c2c368dd3140e09e819092d36b488e0e9
scantero <sm.cantero@gmail.com>
2017-01-18 21:01:21 +0100

    Modificado imprimir y actualizadas las lineas necesarias para que funcione correctamente

--- Fichas.py
+++ Fichas.py
@@ -1,37 +1,48 @@
 class Fichas:
 
-	def __init__(self,c,u1,u2,u3,b1,b2,b3,r1,r2,r3,l1,l2,l3,posSeguidores=[],escudo=False):
+	def __init__(self,c,u1,u2,u3,b1,b2,b3,r1,r2,r3,l1,l2,l3, posSeguidores=[], escudo=False):
 		arriba=[u1,u2,u3]
 		abajo=[b1,b2,b3]
 		derecha=[r1,r2,r3]
 		izquierda=[l1,l2,l3]
 		centro = [c,c,c]
 
-		self.territorio=[arriba,derecha,izquierda,centro,abajo]
+		self.territorio=[centro,arriba,abajo,derecha,izquierda]
 		self.posSeguidores=posSeguidores
 		self.escudo=escudo
 
-	def imprimir(self,posicion):
-		lado=0
-		i=0
+	def imprimir(self,f):
+		ter = f.territorio
+		seg = f.posSeguidores
+		print ter[3]
+		print str(seg[3])
 		resultado = ""
-		for lugar in posicion:
-			if (lado == 0 or lado == 4):
-				while (i < 3):
-					resultado += lugar[i]
-					i=i+1
-				i=0
-			else:
-				resultado+=posicion[2][lado-1]+posicion[3][lado-1]+posicion[1][lado-1]
-			if lado != 4:
-				resultado += "\n"
-
-			lado = lado + 1
-		resultado += "\n"
+		resultado += ter[1][0] + str(seg[3]) + " " + ter[1][1]+ str(seg[4]) + " " + ter[1][2] + str(seg[5]) +"\n"
+		resultado += ter[4][0] + str(seg[12])+ " " + ter[0][0]+ str(seg[0]) + " " + ter[3][0] + str(seg[9]) +"\n"
+		resultado += ter[4][1] + str(seg[13])+ " " + ter[0][1]+ str(seg[1]) + " " + ter[3][1] + str(seg[10])+"\n"
+		resultado += ter[4][2] + str(seg[14])+ " " + ter[0][2]+ str(seg[2]) + " " + ter[3][2] + str(seg[11])+"\n"
+		resultado += ter[2][0] + str(seg[6]) + " " + ter[2][1]+ str(seg[7]) + " " + ter[2][2] + str(seg[8]) +"\n"
 		return resultado
+		# lado=0
+		# i=0
+		# resultado = ""
+		# for lugar in posicion:
+		# 	if (lado == 0 or lado == 4):
+		# 		while (i < 3):
+		# 			resultado += lugar[i]
+		# 			i=i+1
+		# 		i=0
+		# 	else:
+		# 		resultado+=posicion[2][lado-1]+posicion[3][lado-1]+posicion[1][lado-1]
+		# 	if lado != 4:
+		# 		resultado += "\n"
+		#
+		# 	lado = lado + 1
+		# resultado += "\n"
+		# return resultado
 
 
-	def girar(self):
+	def girar(self):#falta modificar incluyendo los seguidores
 		aux_arriba = self.territorio[0]
 		aux_abajo = self.territorio[4]
 		aux_derecha = self.territorio[1]
@@ -40,3 +51,7 @@ class Fichas:
 		self.territorio[1]=aux_arriba
 		self.territorio[2]=aux_abajo
 		self.territorio[4]=aux_derecha
+
+if __name__ == '__main__':
+	f=Fichas('A','A','A','A','P','P','P','A','A','A','A','A','A',[1,0,0,0,0,0,1,0,0,0,0,0,0,0,0])
+	f.imprimir(f)
--- Fichas_test.py
+++ Fichas_test.py
@@ -4,17 +4,17 @@ import unittest
 
 class CarcassoneTest(unittest.TestCase):
 	def test_first_ficha(self):
-		f=Fichas('A','A','A','A','A','A','A','A','A','A','A','A','A',[100000000000000])
+		f=Fichas('A','A','A','A','A','A','A','A','A','A','A','A','A',[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
 		expected="""A0 A0 A0
 A0 A1 A0
 A0 A0 A0
 A0 A0 A0
 A0 A0 A0
 """
-		self.assertEqual(expected,f.imprimir(f.territorio))
+		self.assertEqual(expected,f.imprimir(f))
 
 	def test_second_ficha(self):
-		f=Fichas('A','A','A','A','P','P','P','A','A','A','A','A','A',[100000100000000])
+		f=Fichas('A','A','A','A','P','P','P','A','A','A','A','A','A',[1,0,0,0,0,0,1,0,0,0,0,0,0,0,0])
 		expected="""A0 A0 A0
 A0 A1 A0
 A0 A0 A0
@@ -22,71 +22,71 @@ A0 A0 A0
 P1 P0 P0
 """
 
-		self.assertEqual(expected,f.imprimir(f.territorio))
+		self.assertEqual(expected,f.imprimir(f))
 
 
 	def test_ficha_3(self):
-		f=Fichas('A','P','P','P','P','P','P','A','A','A','A','A','A',[100100100000000])
+		f=Fichas('A','P','P','P','P','P','P','A','A','A','A','A','A',[1,0,0,1,0,0,1,0,0,0,0,0,0,0,0])
 		expected="""P1 P0 P0
 A0 A1 A0
 A0 A0 A0
 A0 A0 A0
 P1 P0 P0
 """
-		self.assertEqual(expected, f.imprimir(f.territorio))
+		self.assertEqual(expected, f.imprimir(f))
 
 	def test_ficha_4(self):
-		f=Fichas('P','A','A','A','P','P','P','A','A','A','P','P','P',[100100000000000])
+		f=Fichas('P','A','A','A','P','P','P','A','A','A','P','P','P',[1,0,0,1,0,0,0,0,0,0,0,0,0,0,0])
 		expected="""A1 A0 A0
-A0 P1 P0
-A0 P0 P0
-A0 P0 P0
+P0 P1 A0
+P0 P0 A0
+P0 P0 A0
 P0 P0 P0
 """
-		self.assertEqual(expected, f.imprimir(f.territorio))
+		self.assertEqual(expected, f.imprimir(f))
 
 	def test_ficha_5(self):
-		f=Fichas('P','A','A','A','P','P','P','P','P','P','A','A','A',[100100000100000])
+		f=Fichas('P','A','A','A','P','P','P','P','P','P','A','A','A',[1,0,0,1,0,0,0,0,0,1,0,0,0,0,0])
 		expected="""A1 A0 A0
-P0 P1 A1
-P0 P0 A0
-P0 P0 A0
+A0 P1 P1
+A0 P0 P0
+A0 P0 P0
 P0 P0 P0
 """
-		self.assertEqual(expected, f.imprimir(f.territorio))
+		self.assertEqual(expected, f.imprimir(f))
 
 	def test_ficha_6(self):
-		f=Fichas('P','P','P','P','P','P','P','A','A','A','A','A','A',[100000000100100])
+		f=Fichas('P','P','P','P','P','P','P','A','A','A','A','A','A',[1,0,0,0,0,0,0,0,0,1,0,0,1,0,0])
 		expected="""P0 P0 P0
 A1 P1 A1
 A0 P0 A0
 A0 P0 A0
 P0 P0 P0
 """
-		self.assertEqual(expected, f.imprimir(f.territorio))
+		self.assertEqual(expected, f.imprimir(f))
 
 	def test_ficha_7(self):
-		f=Fichas('P','A','A','A','P','P','P','P','P','P','P','P','P',[100100000000000])
+		f=Fichas('P','A','A','A','P','P','P','P','P','P','P','P','P',[1,0,0,1,0,0,0,0,0,0,0,0,0,0,0])
 		expected="""A1 A0 A0
 P0 P1 P0
 P0 P0 P0
 P0 P0 P0
 P0 P0 P0
 """
-		self.assertEqual(expected, f.imprimir(f.territorio))
+		self.assertEqual(expected, f.imprimir(f))
 
 	def test_ficha_8(self):
-		f=Fichas('A','A','A','A','P','C','P','A','A','A','A','A','A',[100000111000000])
+		f=Fichas('A','A','A','A','P','C','P','A','A','A','A','A','A',[1,0,0,0,0,0,1,1,1,0,0,0,0,0,0])
 		expected="""A0 A0 A0
 A0 A1 A0
 A0 A0 A0
 A0 A0 A0
 P1 C1 P1
 """
-		self.assertEqual(expected, f.imprimir(f.territorio))
+		self.assertEqual(expected, f.imprimir(f))
 
 
-		self.assertEqual(expected, f.imprimir(f.territorio))
+		self.assertEqual(expected, f.imprimir(f))
 
 
 	def test_long_array(self):
@@ -94,14 +94,14 @@ P1 C1 P1
 		self.assertEqual(len(ArrayFichas().saco),71)
 
 	def test_sacar_ficha_primera(self):
-		f=Fichas('A','A','A','A','A','A','A','A','A','A','A','A','A',[100000000000000])
+		f=Fichas('A','A','A','A','A','A','A','A','A','A','A','A','A',[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
 		expected="""A0 A0 A0
 A0 A1 A0
 A0 A0 A0
 A0 A0 A0
 A0 A0 A0
 """
-		self.assertEqual(f.imprimir(f.territorio),expected)
+		self.assertEqual(f.imprimir(f),expected)
 
 
 	def test_sacar_ult_ficha(self):
@@ -112,7 +112,7 @@ C1 B0 C1
 P0 B0 P0
 P1 C1 P1
 """
-		self.assertEqual(f.imprimir(f.territorio),expected)
+		self.assertEqual(f.imprimir(f),expected)
 
 	def test_sacar_medio_ficha(self):
 		f=ArrayFichas().sacar_ficha(25)
@@ -134,7 +134,7 @@ P0 P0 P0
 
 	def test_girar_ficha(self):
 
-		f=Fichas('A','A','A','A','P','P','P','A','A','A','A','A','A',[100000100000000])
+		f=Fichas('A','A','A','A','P','P','P','A','A','A','A','A','A',[1,0,0,0,0,0,1,0,0,0,0,0,0,0,0])
 		#f=Fichas('A','A','A','A','A','A','A','A','A','A','P','P','P',[100000000000100])
 		f.girar()
 		expected="""A0 A0 A0
