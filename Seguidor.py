4f5ee528a0e4a02c1c5f6a1007dc20a7c80dc269
Diego Payo Martinez <dpayomartinez@gmail.com>
2017-01-18 09:59:26 +0100

    Añado metdo dame_todos() que pasa el test

--- Seguidor.py
+++ Seguidor.py
@@ -13,5 +13,11 @@ class Seguidor:
             return self.to_String(col);
         return "color no existe"
 
+
+    def dame_todos(self):
+        result=""
+        for s in self.color:
+            result+=Seguidor().to_String(s)
+        return result    
     def to_String(self,color):
         return self.seg+" "+color+"\n";
