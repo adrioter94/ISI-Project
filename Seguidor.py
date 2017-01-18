74b61c925cea1cbde36ed9320c9d0aaf8e0eb03a
Diego Payo Martinez <dpayomartinez@gmail.com>
2017-01-18 09:38:55 +0100

    realizado metodo dame_seguidor de la Clase Seguidor que pasa el test Ok

--- Seguidor.py
+++ Seguidor.py
@@ -1,49 +1,17 @@
 import random
 
-class Grupo_segs:
-
-    def __init__(self):
-        self.seg_r=[];
-        self.seg_v=[];
-        self.seg_azu=[];
-        self.seg_ama=[];
-        self.seg_neg=[];
-        self.num=40;
-
-    def rellenar_color (self,color):
-        for col in Color().color:
-            if color== col:
-                for n in range(self.num/len(Color().color)):
-                    self.seg_r.append(Seguidor().dame_seguidor(col))
-
-    def sacar_seg(self,seg):
-        seguidor=self.segs.pop
-        self.segs.remove(seguidor)
-        return seguidor
-
-    def print_saco(self,segs):
-        saco=""
-        for s in segs:
-
-            saco+=s;
-        return saco
-
 class Seguidor:
 
 
     def __init__(self):
         self.seg="S";
         self.color=["rojo","verde","azul","amarillo","negro"];
-        
+
 
     def dame_seguidor(self,col):
-        if col in Color().color:
+        if col in self.color:
             return self.to_String(col);
         return "color no existe"
 
     def to_String(self,color):
         return self.seg+" "+color+"\n";
-
-class Color:
-
-    def __init__(self):
