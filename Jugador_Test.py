fa648759187df457b9edf393110f1564b27abe29
adrioter94 <adrioter94@hotmail.es>
2017-01-19 20:08:54 +0100

    Errores corregido. Funcion OK

--- Jugador_Test.py
+++ Jugador_Test.py
@@ -4,11 +4,14 @@ import unittest
 class JugadorTest(unittest.TestCase):
 
     def test_nuevo_Jugador1(self):
-        j = Jugador('Adrian', 'Rojo');
-        expected = "Nombre: Adrian, Color: Rojo";
+        j = Jugador('Adrian', 'Rojo')
+        expected = "Nombre: Adrian, Color: Rojo"
         self.assertEqual(expected, j.imprimir())
 
     def test_nuevo_Jugador2(self):
-        j = Jugador('Sandra', 'Amarillo');
-        expected = "Nombre: Sandra, Color: Amarillo";
+        j = Jugador('Sandra', 'Amarillo')
+        expected = "Nombre: Sandra, Color: Amarillo"
         self.assertEqual(expected, j.imprimir())
+
+    if __name__ == '__main__':
+        unittest.main()
