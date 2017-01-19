c57777cd817fcbde62ec0d4eb8b7c050548e3e79
adrioter94 <adrioter94@hotmail.es>
2017-01-19 20:01:12 +0100

    Error corregido

--- Jugador_Test.py
+++ Jugador_Test.py
@@ -6,9 +6,9 @@ class JugadorTest(unittest.TestCase):
     def test_nuevo_Jugador1(self):
         j = Jugador('Adrian', 'Rojo');
         expected = "Nombre: Adrian, Color: Rojo";
-        self.assertEqual(expected, j.imprimir());
+        self.assertEqual(expected, j.imprimir())
 
     def test_nuevo_Jugador2(self):
         j = Jugador('Sandra', 'Amarillo');
         expected = "Nombre: Sandra, Color: Amarillo";
-        self.assertEqual(expected, j.imprimir());
+        self.assertEqual(expected, j.imprimir())
