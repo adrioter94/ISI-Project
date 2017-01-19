5525c80ccb415f88e0d7ab8c0e5b9813317b14fc
adrioter94 <adrioter94@hotmail.es>
2017-01-19 19:53:36 +0100

    Clase Jugador

---
+++ Jugador.py
@@ -0,0 +1,10 @@
+class Jugador:
+    def __init__(self, nombre, color):
+        self.nombre = nombre;
+        self.color = color;
+        #Color in ['azul', 'amarillo', 'rojo', 'negro', 'verde']
+        self.turno = False;
+        self.seguidores = 8;
+
+    def imprimir(self):
+        return("Nombre: " + self.nombre + ", Color: " + self.color);
