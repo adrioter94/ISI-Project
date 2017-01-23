6e0bfd06ff1cbebe1cb0fc8277b9cd48c11fbabb
scantero <sm.cantero@gmail.com>
2017-01-23 18:19:36 +0100

    Añadido método pasar_turno() a la clase Partida.py

--- Partida.py
+++ Partida.py
@@ -27,8 +27,17 @@ class Partida:
                  self.jugadores.append(jug)
                  break
 
-if __name__ == '__main__':
-    p = Partida()
-    p.info_jugadores()
-    #for i in range (0, len(p.jugadores)):
-    #    print p.jugadores[i].imprimir()
+    def pasar_turno(self, jugador):
+        """
+        Devuelve el siguiente jugador del array de jugadores.
+        Si es el ultimo del array, devolvera el jugador de la primera posicion.
+        """
+        jugador.turno = False #ya se ha acabado el turno del jugador y tenemos que pasarselo al siguiente de la lista
+        for i in range(4):
+            if self.jugadores[i] == jugador: #hemos encontrado la posicion de ese jugador
+                if i == len(self.jugadores)-1: #si la posicion corresponde al ultimo jugador de la lista
+                    self.jugadores[0].turno = True #el turno vuelve al primero
+                    return self.jugadores[0]
+                else:
+                    self.jugadores[i+1].turno = True #si no es el turno del siguiente jugador de la lista
+                    return self.jugadores[i+1]
