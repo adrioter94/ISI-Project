2bda390f869f1e121430e3006f7a7691699b6ad7
dpayo <color.ui>
2017-01-24 17:35:16 +0100

    segundo test comprueba anadido colores  OK

--- Partida_test.py
+++ Partida_test.py
@@ -12,15 +12,16 @@ class PArtidaTest(unittest.TestCase):
         self.assertEqual(p.jugadores[0].nombre,expected)
         expected="Alberto"
         self.assertEqual(p.jugadores[1].nombre,expected)
-#
-#     # Test para color "azul" -- meter azul de color
-#     def test_info_jugador_color(self):
-#         p=Partida()
-#         p.infoJugadores()
-#         expected="azul"
-#         self.assertEqual(p.jugadores[0].color,expected)
-#
-#
+
+     # Test para color "azul" -- meter azul de color
+    def test_info_jugador_color(self):
+         p=Partida()
+         p.info_jugadores(2,"Adrian","azul","Alberto","verde")
+         expected="azul"
+         self.assertEqual(p.jugadores[0].color,expected)
+         expected="verde"
+         self.assertEqual(p.jugadores[1].color,expected)
+
 # # test anade jugadores al array --- meter 2 jugadores
 #     def test_mete_jugadores (self):
 #       p=Partida()
