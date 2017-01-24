7d77f5b20a554151e334ccb8d778cbf15b2509ff
dpayo <color.ui>
2017-01-24 10:48:31 +0100

    Constructor realizado Ok y test

--- Tablero.py
+++ Tablero.py
@@ -1,41 +1,44 @@
-
+from Array_Fichas import ArrayFichas
 class Tablero:
 
-	def __init__(self):
-		"""
-		Construye una matriz y la rellena con fichas de tipo vacia.
-		"""
-
+    def __init__(self):
+        """
+        Construye una matriz y la rellena con fichas de tipo vacia.
+        """
+    	self.w = 4
+        self.h = 4
+        ficha = ArrayFichas().vacia
+        self.tablero = [[ficha for y in range(0,self.w)] for x in range(0,self.h)]
 
     def insertar(self, ficha, x, y):
 		"""
-		Inserta una ficha en una posición dada por sus dos coordenadas x e y.
+		Inserta una ficha en una posicion dada por sus dos coordenadas x e y.
 		"""
 
 
     def es_valida(self, ficha, x, y):
 		"""
-		Devuelve True si una ficha es válida en una posición del tablero
-		(dada por sus coordenadas x e y) en una orientación dada.
+		Devuelve True si una ficha es valida en una posicion del tablero
+		(dada por sus coordenadas x e y) en una orientacion dada.
 		"""
 
 
     def todas_pos_validas(self, ficha):
 		"""
-		Devuelve un array con todas las posiciones válidas en el tablero
-		para una ficha en una orientación dada.
+		Devuelve un array con todas las posiciones validas en el tablero
+		para una ficha en una orientacion dada.
 		"""
 
 
-    def comprobación_ficha_valida(self, ficha):
+    def comprobacion_ficha_valida(self, ficha):
         """
-		Devuelve True si hay al menos una posición válida a lo largo de todo
-		el tablero para todas las orientaciones de la ficha en cada posición
+		Devuelve True si hay al menos una posicion valida a lo largo de todo
+		el tablero para todas las orientaciones de la ficha en cada posicion
 		(para descartarla directamente).
 		"""
 
 
     def imprimir(self):
 		"""
-		Imprime el tablero en la situación actual.
+		Imprime el tablero en la situacion actual.
 		"""
--- Tablero_test.py
+++ Tablero_test.py
@@ -1,107 +1,20 @@
-from tablero import Tablero
+from Tablero import Tablero
 from Fichas import Fichas
 import unittest
 
 class CarcassoneTest(unittest.TestCase):
-	def test_tablero_filas(self):
-		f=Fichas('C','A','A','A','P','P','P','P','C','P','P','C','P',[1,0,0,1,0,0,1,0,0,1,0,0,0,0,0],False)
-		self.assertEqual(145,len(Tablero(f).tablero))
 
-	def test_tablero_columnas(self):
-		f=Fichas('C','A','A','A','P','P','P','P','C','P','P','C','P',[1,0,0,1,0,0,1,0,0,1,0,0,0,0,0],False)
-		self.assertEqual(145,len(Tablero(f).tablero[0]))
-	def test_tablero_juego_filas(self):
-		f=Fichas('C','A','A','A','P','P','P','P','C','P','P','C','P',[1,0,0,1,0,0,1,0,0,1,0,0,0,0,0],False)
-		self.assertEqual(717,len(Tablero(f).tab_partida))
-	def test_tablero_juego_columnas(self):
-		f=Fichas('C','A','A','A','P','P','P','P','C','P','P','C','P',[1,0,0,1,0,0,1,0,0,1,0,0,0,0,0],False)
-		self.assertEqual(431,len(Tablero(f).tab_partida[0]))
-	def test_coordenadas_validas(self):
-		f=Fichas('C','A','A','A','P','P','P','P','C','P','P','C','P',[1,0,0,1,0,0,1,0,0,1,0,0,0,0,0],False)
-		expected=['7170', '7172', '7271', '7071']
-		coor_validas = Tablero(f).coor_correctas()
-		self.assertEqual(expected,coor_validas)
-	def test_comprobar_extremo_inferior(self):
-		f=Fichas('C','A','A','A','P','P','P','P','C','P','P','C','P',[1,0,0,1,0,0,1,0,0,1,0,0,0,0,0],False)
-		tipo1 = Fichas('A','A','A','A','A','A','A','A','A','A','A','A','A',[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],True)
-		expected = "OK"
-		self.assertEqual(expected,Tablero(f).comprobar_extremos(71,71,tipo1,0,4))
-	def test_comprobar_extremo_superior(self):
-		f=Fichas('C','A','A','A','P','P','P','P','C','P','P','C','P',[1,0,0,1,0,0,1,0,0,1,0,0,0,0,0],False)
-		tipo1 = Fichas('A','A','A','A','A','A','A','A','A','A','A','A','A',[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],True)
-		expected = "ERROR"
-		self.assertEqual(expected,Tablero(f).comprobar_extremos(71,71,tipo1,4,0))
-
-	def test_comprobar_extremo_derecho(self):
-		f=Fichas('C','A','A','A','P','P','P','P','C','P','P','C','P',[1,0,0,1,0,0,1,0,0,1,0,0,0,0,0],False)
-		tipo1 = Fichas('A','A','A','A','A','A','A','A','A','A','A','A','A',[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],True)
-		expected = "ERROR"
-		self.assertEqual(expected,Tablero(f).comprobar_extremos(71,71,tipo1,2,1))
-
-	def test_comprobar_extremo_izquierdo(self):
-		f=Fichas('C','A','A','A','P','P','P','P','C','P','P','C','P',[1,0,0,1,0,0,1,0,0,1,0,0,0,0,0],False)
-		tipo1 = Fichas('A','A','A','A','A','A','A','A','A','A','A','A','A',[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],True)
-		expected = "ERROR"
-		self.assertEqual(expected,Tablero(f).comprobar_extremos(71,71,tipo1,1,2))
-
-	def test_insertar_tabpartida(self):
-		f=Fichas('C','A','A','A','P','P','P','P','C','P','P','C','P',[1,0,0,1,0,0,1,0,0,1,0,0,0,0,0],False)
-		tipo1 = Fichas('A','A','A','A','A','A','A','A','A','A','A','A','A',[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],True)
-		expected="""AAA
-AAA
-AAA
-AAA
-AAA
-AAA
-PCP
-CCC
-PCP
-PPP
+	def test_constructor(self):
+		t=Tablero()
+		expected="""-- -- --
+-- -- --
+-- -- --
+-- -- --
+-- -- --
 """
-		t = Tablero(f)
-		t.insertar(71,70,tipo1)
-		self.assertEqual(expected,t.imprimir_tabpartida())
-
-	def test_insertar_correcta(self):
-		f=Fichas('C','A','A','A','P','P','P','P','C','P','P','C','P',[1,0,0,1,0,0,1,0,0,1,0,0,0,0,0],False)
-		tipo6 = Fichas('P','P','P','P','P','P','P','A','A','A','A','A','A',[1,0,0,0,0,0,0,0,0,1,0,0,1,0,0],False)
-		t = Tablero(f)
-		expected="Ficha insertada en el tablero"
-		self.assertEqual(expected,t.insertar(71,70,tipo6))
-
-	def test_insertar_coor(self):
-		f=Fichas('C','A','A','A','P','P','P','P','C','P','P','C','P',[1,0,0,1,0,0,1,0,0,1,0,0,0,0,0],False)
-		tipo6 = Fichas('P','P','P','P','P','P','P','A','A','A','A','A','A',[1,0,0,0,0,0,0,0,0,1,0,0,1,0,0],False)
-		t = Tablero(f)
-		expected="Coordenas incorrectas"
-		self.assertEqual(expected,t.insertar(41,40,tipo6))
-
-	def test_insertar_ficha_no_valida(self):
-		tipo1 = Fichas('A','A','A','A','A','A','A','A','A','A','A','A','A',[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],True)
-		tipo16 = Fichas('I','P','P','P','P','P','P','P','P','P','P','P','P',[1,0,0,1,0,0,0,0,0,0,0,0,0,0,0],False)
-		t = Tablero(tipo1)
-		expected="No valida"
-		self.assertEqual(expected,t.insertar(71,70,tipo16))
-
-	def test_insertar(self):
-		f=Fichas('C','A','A','A','P','P','P','P','C','P','P','C','P',[1,0,0,1,0,0,1,0,0,1,0,0,0,0,0],False)
-		tipo1 = Fichas('A','A','A','A','A','A','A','A','A','A','A','A','A',[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],True)
-		t = Tablero(f)
-		t.insertar(71,70,tipo1)
-		tipo18 = Fichas('C','P','C','P','P','C','P','P','P','P','P','P','P',[1,0,0,1,0,1,0,0,0,0,0,0,0,0,0],False)
-		t.insertar(72,71,tipo18)
-		expected="""AAA
-AAA
-AAA
-AAA
-AAA
-AAAPPP
-PCPPCP
-CCCCCC
-PCPPCP
-PPPPPP
-"""
-		self.assertEqual(expected,t.imprimir_tabpartida())
+		for y in range(0,t.w): #w columnas
+			 for x in range(0,t.h): # h filas
+				 self.assertEqual(expected,t.tablero[x][y].imprimir())
 
 if __name__ == '__main__':
 	unittest.main()
