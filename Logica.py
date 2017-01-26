0b32c390c002258ed630cc3a9610587c37e56099
scantero <sm.cantero@gmail.com>
2017-01-26 18:03:08 +0100

    Clase Logica.py y constructor completados

---
+++ Logica.py
@@ -0,0 +1,11 @@
+from Array_Fichas import ArrayFichas
+from Tablero import Tablero
+from Fichas import Fichas
+
+class Logica:
+
+    def __init__(self):
+        self.array_caminos = []
+        self.array_aldeas = []
+
+        
