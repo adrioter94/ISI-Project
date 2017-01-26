a386078c1d66a679949572ee1cd5ed106cc9ac3f
dpayo <color.ui>
2017-01-26 12:15:26 +0100

    Test Tablero y Partida Ok

--- Tablero_test.py
+++ Tablero_test.py
@@ -5,6 +5,7 @@ import unittest
 
 class CarcassoneTest(unittest.TestCase):
 
+    # Long saco (0-70)
     # test constructor ficha inicial
     def test_constructor(self):
         t=Tablero()
@@ -30,32 +31,30 @@ A0 A0 A0
 
     def test_es_valida(self):
         t = Tablero()
-        ficha1 = ArrayFichas().tipo1 #Todo aldea
+        ficha1 = ArrayFichas().sacar_ficha(0) #Todo aldea
         t.insertar(ficha1, 1, 1)
-        ficha2 = ArrayFichas().tipo19
-        ficha3 = ArrayFichas().tipo2
+        ficha2 = ArrayFichas().sacar_ficha(70) #tipo19
+        ficha3 = ArrayFichas().sacar_ficha(1)  #tipo2
         self.assertFalse(t.es_valida(ficha2, 1, 2))
         self.assertTrue(t.es_valida(ficha3, 1, 2))
 
     def test_todas_pos_validas(self):
         t = Tablero()
-        ficha1 = ArrayFichas().tipo1 #Todo aldea
+        ficha1 = ArrayFichas().sacar_ficha(0) #Todo aldea
         t.insertar(ficha1, 2, 2)
-        ficha2 = ArrayFichas().tipo7 #Solo un lado con aldea
+        ficha2 = ArrayFichas().sacar_ficha(22) #Solo un lado con aldea
         expected = [[3, 2]]
         self.assertEqual(expected, t.todas_pos_validas(ficha2))
 
+
+    # hay que contar que ya esta puesta la ficha inicial
     def test_comprobacion_ficha_valida(self):
         t = Tablero()
-        ficha1 = ArrayFichas().tipo7 #Aldea por arriba y el resto pradera
+        ficha1 = ArrayFichas().sacar_ficha(21) # tipo7 Aldea por arriba y el resto pradera
         t.insertar(ficha1, 2, 2)
-        ficha2 = ArrayFichas().tipo8 #Aldea por 3 lados y camino por abajo
-        ficha3 = ArrayFichas().tipo15 #Todo camino
-        print t.imprimir()
+        ficha2 = ArrayFichas().sacar_ficha(24) # tipo8 Aldea por 3 lados y camino por abajo
+        ficha3 = ArrayFichas().sacar_ficha(48) #tipo 15Todo camino
         self.assertTrue(t.comprobacion_ficha_valida(ficha2))
-
-
-
 
 
 
