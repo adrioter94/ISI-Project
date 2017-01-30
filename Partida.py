8439043374ce20969a49530af6fea61b011e4ca4
arotena <albertoss27@hotmail.com>
2017-01-30 16:30:33 +0100

    correccion de errores y modificacion de la funcion jugar turno

--- Partida.py
+++ Partida.py
@@ -73,37 +73,6 @@ class Partida:
                     return self.jugadores[i+1]
 
 
-    def pos_valida(self, pos_validas, pos):
-        # Devuelve True si la pos pasada como parametro se encuentra dentro del array pos_validas
-        response = False
-        for i in pos_validas:
-            if str(i) != pos:
-                continue
-            else:
-                response = True
-        return response
-
-
-    def elegir(self, pos_validas, eleccion=None):
-        #Comprueba que la eleccion del jugador en cada turno sea valida,
-        #es decir, que elija una posicion valida donde colocar la ficha
-        #o girarla.
-        error = False
-        print "Posiciones Validas: "
-        for i in pos_validas:
-            print i
-        print "Girar (G)"
-
-        while eleccion != "G" and not self.pos_valida(pos_validas, eleccion):
-            print "Formatos validos: [x, y] || G. Por favor vuelve a intentarlo."
-            print "Posiciones Validas: "
-            for i in pos_validas:
-                print i
-            print "Girar (G)"
-
-        return eleccion
-
-
     def colocar_seguidor(self,ficha,jugador,indice):
         #coloca un seguidor del color del jugador que se le pasa como parametro
         #si en esa posicion se puede colocar un seguidor
@@ -142,8 +111,9 @@ class Partida:
                 if u.zona[6 + i] in colores:
                     zona1 = ficha.zona[3 + i]
                     if zona1 in colores:
-                        return
-                    ficha.pintar_ficha(zona1,u.zona[6 + i])
+                        pass
+                    else:
+                        ficha.pintar_ficha(zona1,u.zona[6 + i])
                 i += 1
         i = 0
         if b.territorio[0][1] != '-':
@@ -151,8 +121,9 @@ class Partida:
                 if b.zona[3 + i] in colores:
                     zona1 = ficha.zona[6 + i]
                     if zona1 in colores:
-                        return
-                    ficha.pintar_ficha(zona1,b.zona[3 + i])
+                        pass
+                    else:
+                        ficha.pintar_ficha(zona1,b.zona[3 + i])
                 i += 1
         i = 0
         if r.territorio[0][1] != '-':
@@ -160,8 +131,9 @@ class Partida:
                 if r.zona[12 + i] in colores:
                     zona1 = ficha.zona[9 + i]
                     if zona1 in colores:
-                        return
-                    ficha.pintar_ficha(zona1,r.zona[12 + i])
+                        pass
+                    else:
+                        ficha.pintar_ficha(zona1,r.zona[12 + i])
                 i += 1
         i = 0
         if l.territorio[0][1] != '-':
@@ -169,8 +141,9 @@ class Partida:
                 if l.zona[9 + i] in colores:
                     zona1 = ficha.zona[12 + i]
                     if zona1 in colores:
-                        return
-                    ficha.pintar_ficha(zona1,l.zona[9 + i])
+                        pass
+                    else:
+                        ficha.pintar_ficha(zona1,l.zona[9 + i])
                 i += 1
 
 
@@ -197,42 +170,27 @@ class Partida:
             i = i + 1
             z = 0
 
-    def jugar_turno(self, jugador):
+    def jugar_turno(self, jugador, posicion_elegida, quiere_seguidor, ficha, posicion_seguidor=None):
         #Cada vez que sea el turno de un jugador:
-        #1)  Sacara una ficha del saco.
-        #2)  Se comprobara si esa ficha se puede colocar en el tablero, si no se puede se sacara otra ficha.
-        #3)  Si se puede colocar en alguna posicion del tablero, se le mostrara al jugador para que elija
-        #    las posiciones disponibles para esa ficha colocada tal cual esta, o bien puede elegir 'Girarla'.
-        #4)  Si elige 'Girarla', de nuevo se recalcularan las posiciones en las que se puede colocar esa ficha
-        #    girada y se le mostraran para que elija entre las posiciones disponibles, o 'Girarla' de nuevo.
-        #5)  En el momento en el que elija colocar la ficha en una de las posiciones mostradas, se recalcularan
-        #    las posiciones de los seguidores para esa ficha teniendo en cuenta la posicion que ocupa dentro del
-        #    tablero (p.e. no se pueden poner dos granjeros del mismo jugador dentro de la misma pradera).
-        #6)  Con las posiciones posibles de los seguidores actualizadas, se le da al jugador la posibilidad de
-        #    colocar un seguidor en su turno, si dispone de seguidores para colocar.
-        #7)  Se comprobara si hay que sumarle puntos al jugador en ese turno.
-        #8)  El turno pasara al siguiente jugador.
-
-        while len(self.saco) <= 71:
-            ficha = self.saco.sacar_ficha()
-            if not self.tablero.comprobacion_ficha_valida(ficha): #valida para todos los giros en todas las posiciones que hay disponibles
-                continue
-            while True:
-                pos_validas = self.tablero.todas_pos_validas(ficha) #posiciones validas para una posicion concreta (NO todos los giros)
-                eleccion = self.elegir(pos_validas) #pinta las posiciones validas y 'Girar' para que el jugador elija
-                if eleccion == 'G':
-                    ficha.girar()
-                    continue
-                elif eleccion not in pos_validas:
-                    print "No hay ninguna posicion valida que coincida con tu eleccion."
-                    continue
-                self.actualizar_zona(ficha, eleccion)
-                self.saco.eliminar_ficha(ficha)
-                if jugador.seguidores != 0:
-                    ficha = self.colocar_seguidores(ficha, jugador) #te devuelve una ficha con el vector pos_seguidores actualizado
-                self.tablero.insertar(eleccion[0], eleccion[1], ficha)
-                self.algoritmo_relleno(eleccion[0],eleccion[1])
-                #self.computar_puntos_turno(tablero, ficha, jugador)
-                next_jugador = self.pasar_turno(jugador)
-                break
-            self.jugar_turno(next_jugador)
+        #1)  La idea es hacer un test llamando a este metodo 72 veces porque hay 72 piezas y nosotros le vamos pasando
+		# la posicion elegida a mano que en caso de juego original nos la tandria que pasar la interfaz grafica,
+		# y la ficha que nos pasaria tendria que ser valida porque la interfaz grafica se encargara de probar que es valida
+        # esa ficha antes de mostrarla y la posicion que nos pase tambien tiene que ser valida porque la interfaz
+        # grafica solo nos deberia dejar poner la ficha en posiciones validas.
+        # Si el jugador quiere girar la ficha deberia tener un boton la interfaz grafica que se
+        # encarge de girar la ficha llamando al metodo girar de la clase ficha y ya nos pasaria la ficha a este metodo
+        #
+        #Y luego si quiere colocar seguidor que nos pase un "SI" o un "NO" en la variable quiere_seguidor
+		# en el caso de que sea que "SI" que nos pase tambien su posicion.
+        pos_validas = self.tablero.todas_pos_validas(ficha) #posiciones validas para una posicion concreta (NO todos los giros)
+        if posicion_elegida in pos_validas:
+            self.actualizar_zona(ficha, posicion_elegida)
+            self.saco.eliminar_ficha(ficha)
+            if jugador.seguidores != 0:
+                if quiere_seguidor == "SI":
+                    self.colocar_seguidor(ficha, jugador,posicion_seguidor)
+            self.tablero.insertar(ficha,posicion_elegida[0], posicion_elegida[1])
+            self.algoritmo_relleno(posicion_elegida[0], posicion_elegida[1])
+            self.pintada_false()
+            return "OK"
+        return "ERROR"
